88de59be49cb03a14f1a2aa70d2c0f876bffafe5
Sanjit Kumar <sanjitkmr112@gmail.com>
2026-09-15 15:20:56 -0500

    lab 3 starter files added

---
+++ lab3/lab3_provided_fa24-2/design_source/ripple_adder.sv
@@ -0,0 +1,16 @@
+module ripple_adder (
+	input  logic  [15:0] a, 
+    input  logic  [15:0] b,
+	input  logic         cin,
+	
+	output logic  [15:0] s,
+	output logic         cout
+);
+
+	/* TODO
+		*
+		* Insert code here to implement a ripple adder.
+		* Your code should be completly combinational (don't use always_ff or always_latch).
+		* Feel free to create sub-modules or other files. */
+    
+endmodule
---
+++ lab3/lab3_provided_fa24-2/design_source/select_adder.sv
@@ -0,0 +1,16 @@
+module select_adder (
+	input  logic  [15:0] a, 
+    input  logic  [15:0] b,
+	input  logic         cin,
+	
+	output logic  [15:0] s,
+	output logic         cout
+);
+
+	/* TODO
+		*
+		* Insert code here to implement a CSA adder.
+		* Your code should be completly combinational (don't use always_ff or always_latch).
+		* Feel free to create sub-modules or other files. */
+
+endmodule

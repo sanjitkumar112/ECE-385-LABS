0fd8731a3993ea988ebce747ab0714665c146e4a
amogk <amogkpatil@gmail.com>
2026-09-16 23:46:29 -0500

    Lab3 commits

---
+++ lab3/Lab 3 vivado/Lab 3 vivado.srcs/sources_1/new/four_bit_ripple_adder.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/16/2026 10:45:53 PM
+// Design Name: 
+// Module Name: four_bit_ripple_adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module four_bit_ripple_adder(
+    input [3:0] a, 
+    input [3:0] b,
+	input cin,
+	
+	output [3:0] s,
+	output cout
+);
+
+    wire [3:0] c;
+    assign c[0] = cin; 
+    assign cout = c[4];
+    
+    genvar i;
+    for (i = 0; i < 4; i = i+1) begin:ra
+        single_ripple sr (
+            .a (a[i]),
+            .b (b[i]),
+            .cin (c[i]),
+            .s (s[i]),
+            .c_out (c[c[i+1]])
+        );
+    end
+
+endmodule
